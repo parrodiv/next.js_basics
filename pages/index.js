70d86160daca72dac005d96fa9c14f49d8f8c25c
Alessandro Parrilla <paggio96@gmail.com>
2023-02-20 13:45:08 +0100

    routing pages index --> '/' about.js --> /about

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home () {
+  return (
+    <>
+      <h1>Welcome to next</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eius sequi cupiditate, fugit sunt laborum voluptatum modi alias vitae nam?</p>
+    </>
+  )
+}
