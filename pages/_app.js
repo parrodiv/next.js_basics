c31ab86394ca4f5b146a731ce21215589941143e
Alessandro Parrilla <alessandroparrilla@MBPdiAlessandro.station>
2023-02-24 14:55:05 +0100

    App-wide code improved for more features check out Layout on Next.js documentation, Navigation implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,35 @@
+import '../styles/global.css'
+import Link from 'next/link'
+import {useRouter} from 'next/router'
 
-const App = (props) => {
+const App = ({Component, pageProps}) => {
+  const router = useRouter()
+  console.log(router.pathname)
+
   return (
     <>
+      <div>
+        <h1>Our site</h1>
+        <nav className='header-nav'>
+          <ul>
+            <li>
+              <Link className={router.pathname == '/' ? 'active' : ''} href='/'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={router.pathname == '/about' ? 'active' : ''}
+                href='/about'
+              >
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <p>Header</p>
-      <props.Component></props.Component>
+      <Component {...pageProps} />
       <p>Footer</p>
     </>
   )
